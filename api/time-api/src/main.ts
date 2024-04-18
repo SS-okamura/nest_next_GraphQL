12f38d7a79c177d6a17f9a55826d65583bc1595c
SS-okamura <ss.okamura0509@gmail.com>
2024-04-18 19:01:38 +0900

    validation back

--- api/time-api/src/main.ts
+++ api/time-api/src/main.ts
@@ -1,11 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './main/app.module';
 import { PrismaService } from './infra/prisma/services/prisma.service';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
   await app.listen(3000);
