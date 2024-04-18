337d0d7b1c033f780235c11eb34c7f3117c74ecf
SS-okamura <ss.okamura0509@gmail.com>
2024-04-18 11:23:33 +0900

    不要なファイルの削除

--- api/time-api/src/components/posts/posts.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PostsResolver } from './post.resolvers';
-import { PostsService } from 'src/components/posts/posts.service';
-import { PrismaService } from 'src/infra/prisma/services/prisma.service';
-
-@Module({
-  providers: [PostsResolver, PostsService, PrismaService],
-})
-export class PostsModule {}
--- api/time-api/src/components/users/users.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UsersResolver } from './users.resolvers';
-import { UsersService } from './users.service';
-import { PrismaService } from 'src/infra/prisma/services/prisma.service';
-
-@Module({
-  providers: [UsersResolver, PrismaService, UsersService],
-})
-export class UsersModule {}
