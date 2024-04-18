25d57249f5423acce188ac4309dbd1c5c639b56b
SS-okamura <ss.okamura0509@gmail.com>
2024-04-18 11:23:05 +0900

    DB 更新

--- api/time-api/prisma/migrations/20240417050642_add_role_password_to_user/migration.sql
+++ api/time-api/prisma/migrations/20240417050642_add_role_password_to_user/migration.sql
@@ -8,5 +8,5 @@
 CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN');
 
 -- AlterTable
-ALTER TABLE "users" ADD COLUMN     "adminFlg" "Role" NOT NULL DEFAULT 'USER',
+ALTER TABLE "users" ADD COLUMN     "role" "Role" NOT NULL DEFAULT 'USER',
 ADD COLUMN     "password" TEXT NOT NULL;
